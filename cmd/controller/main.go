4a711c4f635950fd0096341fdceaa2416998483c
root <root@ks2-ksr-170.cluster.local>
2020-09-03 20:04:45 +0800

    implment controllers

---
+++ cmd/controller/main.go
@@ -0,0 +1,111 @@
+package main
+
+import (
+    "flag"
+    "os"
+    "time"
+    "path/filepath"
+
+    "k8s.io/klog"
+    "k8s.io/client-go/kubernetes"
+    "k8s.io/client-go/tools/clientcmd"
+    "k8s.io/client-go/tools/cache"
+    "k8s.io/client-go/tools/record"
+    "k8s.io/client-go/util/workqueue"
+
+    corev1 "k8s.io/api/core/v1"
+    typedcorev1 "k8s.io/client-go/kubernetes/typed/core/v1"
+    utilruntime "k8s.io/apimachinery/pkg/util/runtime"
+    apiextensionsclientset "k8s.io/apiextensions-apiserver/pkg/client/clientset/clientset"
+
+    xscheme      "github.com/chiuminghan/kube-database/pkg/client/clientset/versioned/scheme"
+    xclientset   "github.com/chiuminghan/kube-database/pkg/client/clientset/versioned"
+    xlister      "github.com/chiuminghan/kube-database/pkg/client/listers/example.com/v1"
+    xinformers   "github.com/chiuminghan/kube-database/pkg/client/informers/externalversions"
+    xapi          "github.com/chiuminghan/kube-database/pkg/apis/example.com/v1"
+)
+
+type Controller struct {
+    kubeclientset          kubernetes.Interface
+    apiextensionsclientset apiextensionsclientset.Interface
+    xclientset             xclientset.Interface
+    informer               cache.SharedIndexInformer
+    lister                 xlister.DatabaseLister
+    recorder               record.EventRecorder
+    workqueue              workqueue.RateLimitingInterface
+}
+
+func NewController() *Controller {
+
+    var kubeconfig *string
+    if home := homeDir(); home != "" {
+        kubeconfig = flag.String("kubeconfig", filepath.Join(home, ".kube", "config"), "(optional) absolute path to the kubeconfig file")
+    } else {
+        kubeconfig = flag.String("kubeconfig", "", "absolute path to the kubeconfig file")
+    }
+    flag.Parse()
+
+    config, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
+    if err != nil {
+        panic(err.Error())
+    }
+
+    kubeClient := kubernetes.NewForConfigOrDie(config)
+    apiextensionsClient := apiextensionsclientset.NewForConfigOrDie(config)
+    myClient := xclientset.NewForConfigOrDie(config)
+
+    informerFactory := xinformers.NewSharedInformerFactory(myClient, time.Minute*1)
+    informer := informerFactory.Example().V1().Databases()
+
+    utilruntime.Must(xapi.AddToScheme(xscheme.Scheme))
+    eventBroadcaster := record.NewBroadcaster()
+    eventBroadcaster.StartLogging(klog.Infof)
+    eventBroadcaster.StartRecordingToSink(&typedcorev1.EventSinkImpl{Interface: kubeClient.CoreV1().Events("")})
+    recorder := eventBroadcaster.NewRecorder(xscheme.Scheme, corev1.EventSource{Component: "database-controller"})
+
+    workqueue := workqueue.NewRateLimitingQueue(workqueue.DefaultControllerRateLimiter())
+
+    c := &Controller{
+        kubeclientset:          kubeClient,
+        apiextensionsclientset: apiextensionsClient,
+        xclientset:             myClient,
+        informer:               informer.Informer(),
+        lister:                 informer.Lister(),
+        recorder:               recorder,
+        workqueue:              workqueue,
+    }
+
+   return c
+}
+
+func (c *Controller) Run(){
+    defer utilruntime.HandleCrash()
+    defer c.workqueue.ShutDown()
+
+    klog.Infoln("Waiting cache to be synced.")
+    // Handle timeout for syncing.
+    timeout := time.NewTimer(time.Second * 6000)
+    timeoutCh := make(chan struct{})
+    go func() {
+        <-timeout.C
+        timeoutCh <- struct{}{}
+    }()
+    if ok := cache.WaitForCacheSync(timeoutCh, c.informer.HasSynced); !ok {
+        klog.Fatalln("Timeout expired during waiting for caches to sync.")
+    }
+
+    klog.Infoln("Starting custom controller.")
+    select {}
+}
+
+func main() {
+    controller := NewController()
+    controller.Run()
+}
+
+func homeDir() string {
+    if h := os.Getenv("HOME"); h != "" {
+        return h
+    }
+    return os.Getenv("USERPROFILE") // windows
+}
--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/imdario/mergo v0.3.11 // indirect
 	golang.org/x/time v0.0.0-20200630173020-3af7569d3a1e // indirect
 	k8s.io/api v0.19.0 // indirect
+	k8s.io/apiextensions-apiserver v0.0.0-00010101000000-000000000000 // indirect
 	k8s.io/apimachinery v0.19.0
 	k8s.io/client-go v11.0.0+incompatible
 	k8s.io/utils v0.0.0-20200821003339-5e75c0163111 // indirect
